d5380db2570b5e2bab69b8c14bb92cd29b1473ce
scjpdba <scjpdba@naver.com>
2020-09-11 15:00:27 +0900

    Hello, R

---
+++ hello.R
@@ -0,0 +1,12 @@
+# 스크립트 전체 실행 : Ctl + Alt + R
+# 한줄 실행 : Ctl + Enter
+
+# 작업 디렉토리 확인 및 설정
+getwd()
+setwd("C:/work/RProject/rstatistics/")
+# R Source 실행
+source("PrintHello.R")
+
+# 상위 디렉토리로 이동
+setwd("..")
+getwd()
